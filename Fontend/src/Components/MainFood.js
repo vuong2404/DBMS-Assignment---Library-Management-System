94bbb66212c839f5d51cbb7d7748a4eb50946a90
Trần Đại Quý <105071667+daiquydev@users.noreply.github.com>
2024-04-21 19:51:44 +0700

    Update MainFood.js

--- Fontend/src/Components/MainFood.js
+++ Fontend/src/Components/MainFood.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import Allfood from "./MyData/FoodData"
+import React, { useState, useEffect } from 'react'
 import OurFood from './OurFood'
 
 const MainFood = () => {
+  const [foodData, setFoodData] = useState([])
 
-    console.log(Allfood);
+  useEffect(() => {
+    fetch('http://localhost:3001/api/all')
+      .then((response) => response.json())
+      .then((data) => setFoodData(data))
+      .catch((error) => console.error('Error fetching food data:', error))
+  }, [])
+
   return (
-    <>
-      <>
-       <div className='container-fluid'>
-          <div className='row d-flex justify-content-between'>
-               {
-                Allfood.map((curr,i)=>{
-                    return <OurFood food ={i} {...curr}/>
-                })
-               }
-          </div>
-       </div>
-      </>
-    </>
+    <div className="container-fluid">
+      <div className="row d-flex justify-content-between">
+        {foodData.map((food, i) => (
+          <OurFood key={i} {...food} />
+        ))}
+      </div>
+    </div>
   )
 }
 
