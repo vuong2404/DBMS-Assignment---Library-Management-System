0715e3dcebd36b190dd3ee86e3c8728a118fcd5d
Trần Đại Quý <105071667+daiquydev@users.noreply.github.com>
2024-04-21 19:53:30 +0700

    Update App.js

--- Fontend/src/App.js
+++ Fontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./Components/Navbar";
-import SlidingImage from "./Components/SlidingImage";
-import OurDelivery from "./Components/OurDelivery";
-import Footer from "./Components/Footer";
-import CategoryProduct from "./Components/CategoryProduct";
-import FoodCategory from "./Components/FoodCategory";
-import Ourfood from "./Components/OurFood";
-import OurFood from "./Components/OurFood";
-import MainFood from "./Components/MainFood";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Components/Home";
-import About from "./Components/About";
+import logo from './logo.svg'
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './Components/Navbar'
+import SlidingImage from './Components/SlidingImage'
+import OurDelivery from './Components/OurDelivery'
+import Footer from './Components/Footer'
+import CategoryProduct from './Components/CategoryProduct'
+import FoodCategory from './Components/FoodCategory'
+import Ourfood from './Components/OurFood'
+import OurFood from './Components/OurFood'
+import MainFood from './Components/MainFood'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Components/Home'
+import About from './Components/About'
 
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import { AuthProvider } from "./AuthContext";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import { AuthProvider } from './AuthContext'
 
 function App() {
   return (
@@ -30,7 +30,7 @@ function App() {
             <Route path="/Register" element={<Register />} />
             <Route path="/Login" element={<Login />} />
             <Route path="/" element={<Home />} />
-            <Route path="/OurFood" element={<MainFood />} />
+            <Route path="/OurBook" element={<MainFood />} />
             <Route path="/About" element={<About />} />
           </Routes>
           <Footer />
@@ -38,7 +38,7 @@ function App() {
         </AuthProvider>
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
